b419b47437096085d8e590bf863a901e52c6836e
Adelina609 <adeliachar@gmail.com>
2018-10-22 00:07:22 +0400

    homework_6 update

--- app/src/main/java/com/example/hw3_b/AlarmReceiver.java
+++ app/src/main/java/com/example/hw3_b/AlarmReceiver.java
@@ -1,76 +1,34 @@
 package com.example.hw3_b;
 
-import android.app.Notification;
+
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.media.Ringtone;
 import android.media.RingtoneManager;
-import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
-import android.widget.Toast;
 
-//import android.support.v4.content.WakefulBroadcastReceiver;
 public class AlarmReceiver extends BroadcastReceiver {
 
+    final String ID = "5";
+
     @Override
-    public void onReceive(final Context context, Intent intent1) {
-//        Intent intent1 = new Intent(context, MainActivity.class);
-//        intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        mn.sendNotification("title", "body", intent, 0);
-//        intent1 = new Intent(context, MainActivity.class);
-//        intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        Toast.makeText(context, "..................", Toast.LENGTH_LONG).show();
-//
-//        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-//        Ringtone ringtone = RingtoneManager.getRingtone(context, uri);
-//        ringtone.play();
-//
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-//                .setSmallIcon(R.drawable.ic_menu_gallery)
-//                .setContentTitle("mmmmmmm")
-//                .setContentText("bodyy");
-//
-//        intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent1,
-//                    PendingIntent.FLAG_ONE_SHOT);
-//        builder.setContentIntent(pendingIntent);
-//
-//
-//        NotificationManager notificationManager = (NotificationManager)context
-//                .getSystemService(Context.NOTIFICATION_SERVICE);
-//        notificationManager.notify(0, builder.build());
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent pendingIntent =  PendingIntent.getActivity(context, 0, intent, 0);
-        AlarmService alarmService = new AlarmService(context, pendingIntent);
-//        alarmService.createNotifChannel();
-        alarmService.sendNotification();
-        Toast.makeText(context, "BITCHBITCH", Toast.LENGTH_LONG).show();
+    public void onReceive(Context context, Intent intent) {
+
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(ID, "channel1", importance);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+            channel.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION),
+                    channel.getAudioAttributes());
+        }
+        NotificationService notificationService = new NotificationService();
+        notificationManager.notify(1, notificationService.sendNotification(context));
     }
-
-//    public void sendNotification(String title, String body, Intent intent, int pushid){
-//        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-//                .setSmallIcon(R.drawable.ic_menu_gallery)
-//                .setContentTitle(title)
-//                .setSound(defaultSound)
-//                .setContentText(body);
-//
-//        if(intent!=null){
-//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
-//                    PendingIntent.FLAG_ONE_SHOT);
-//            builder.setContentIntent(pendingIntent);
-//        }
-//
-//        NotificationManager notificationManager = (NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
-//        notificationManager.notify(0, builder.build());
-//    }
-
-
-
-    }
+}
--- app/src/main/java/com/example/hw3_b/MainActivity.java
+++ app/src/main/java/com/example/hw3_b/MainActivity.java
@@ -1,109 +1,86 @@
 package com.example.hw3_b;
-import java.util.Calendar;
 import android.app.AlarmManager;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.NotificationCompat;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity{
-    private static int timeHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-    private static int timeMinute = Calendar.getInstance().get(Calendar.MINUTE);
-    TextView textView1;
-    private TextView textView2;
-    AlarmManager alarmManager;
+import com.example.notification.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etMin;
+    private EditText etSec;
+    private TextView tvLeft;
+    private AlarmManager alarmManager;
     private PendingIntent pendingIntent;
+    private CountDownTimer timer;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mylayout);
-        textView1 = (TextView)findViewById(R.id.msg1);
-        textView1.setText(timeHour + ":" + timeMinute);
-        textView2 = (TextView)findViewById(R.id.msg2);
+        setContentView(R.layout.activity_main);
 
-        Intent myIntent = new Intent(MainActivity.this, AlarmReceiver.class);
-        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, 0);
+        etMin =  findViewById(R.id.et_time_minutes);
+        etSec = findViewById(R.id.et_time_sec);
+        Button btnStart = findViewById(R.id.btn_start);
+        Button btnStop = findViewById(R.id.btn_stop);
+        tvLeft = findViewById(R.id.tv_timer);
 
-        OnClickListener listener1 = new OnClickListener() {
-            public void onClick(View view) {
-                textView2.setText("");
-                Bundle bundle = new Bundle();
-                bundle.putInt(MyConstants.HOUR, timeHour);
-                bundle.putInt(MyConstants.MINUTE, timeMinute);
-                MyDialogFragment fragment = new MyDialogFragment(new MyHandler());
-                fragment.setArguments(bundle);
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                transaction.add(fragment, MyConstants.TIME_PICKER);
-                transaction.commit();
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
             }
-        };
+        });
 
-        Button btn1 = (Button)findViewById(R.id.button1);
-        btn1.setOnClickListener(listener1);
-        OnClickListener listener2 = new OnClickListener() {
-            public void onClick(View view) {
-                textView2.setText("");
-                cancelAlarm();
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelTimer();
+                tvLeft.setText("");
             }
-        };
-        Button btn2 = (Button)findViewById(R.id.button2);
-        btn2.setOnClickListener(listener2);
+        });
     }
 
-    class MyHandler extends Handler {
-        @Override
-        public void handleMessage (Message msg){
-            Bundle bundle = msg.getData();
-            timeHour = bundle.getInt(MyConstants.HOUR);
-            timeMinute = bundle.getInt(MyConstants.MINUTE);
-            textView1.setText(timeHour + ":" + timeMinute);
-            setAlarm();
-        }
-    }
-    private void setAlarm(){
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, timeHour);
-        calendar.set(Calendar.MINUTE, timeMinute);
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-    }
-    private void cancelAlarm() {
-        if (alarmManager!= null) {
-            alarmManager.cancel(pendingIntent);
-        }
+
+    private void start() {
+        long timeOnTimer = Integer.parseInt(etMin.getText().toString()) * 60000 +
+                Integer.parseInt(etSec.getText().toString()) * 1000;
+        start(timeOnTimer);
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intentTimer = new Intent(this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast
+                (this, 0, intentTimer, PendingIntent.FLAG_CANCEL_CURRENT);
+        alarmManager.set(AlarmManager.RTC_WAKEUP,
+                System.currentTimeMillis() + timeOnTimer, pendingIntent);
     }
 
-//    public void sendNotification(String title, String body, Intent intent, int pushid){
-//        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-//                .setSmallIcon(R.drawable.ic_menu_gallery)
-//                .setContentTitle(title)
-//                .setSound(defaultSound)
-//                .setContentText(body);
-//
-//        if(intent!=null){
-//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
-//                    PendingIntent.FLAG_ONE_SHOT);
-//            builder.setContentIntent(pendingIntent);
-//        }
-//
-//        NotificationManager notificationManager = (NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
-//        notificationManager.notify(0, builder.build());
-//    }
+    private void start(final long timeOnTimer) {
+        timer = new CountDownTimer(timeOnTimer, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tvLeft.setText("" + millisUntilFinished / 60000 + " min " + (millisUntilFinished % 60000) / 1000 + " sec"
+                + " left");
+            }
 
+            @Override
+            public void onFinish() {
+                Toast.makeText(getApplicationContext(), "Yeeeey", Toast.LENGTH_LONG)
+                        .show();
+            }
+        } .start();
+    }
+
+    private void cancelTimer() {
+        alarmManager.cancel(pendingIntent);
+        timer.cancel();
+    }
 }
